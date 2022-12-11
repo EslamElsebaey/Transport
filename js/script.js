04420da208d16432444ecc0b1cd25e9c8a412139
EslamElsebaey <eslamelsebaey.11@gmail.com>
2022-12-11 14:08:55 +0200

    edit

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ $(window).scroll(function(){
 })
 
 $(".toTop").click(function(){
-  $('html, body').animate({scrollTop:0}, '1000');
+  $('html, body').animate({scrollTop:0}, 400);
 }) 
 
 /************************************************************************ */
